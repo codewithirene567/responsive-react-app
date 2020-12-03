3f135fcbd0273b5687bc0725eaa532f215999274
codewithirene567 <irenejinscott@gmail.com>
2020-12-03 17:43:15 -0500

    commented out a line to change the video in background, added further Herosection logic, put imports in

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from
 'react-router-dom';
+import Home from './components/pages/Home';
 
 
 function App() {
@@ -11,7 +12,8 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path="/" exact />
+          <Route path='/' exact component=
+          {Home} />
         </Switch>
       </Router>
     </>
