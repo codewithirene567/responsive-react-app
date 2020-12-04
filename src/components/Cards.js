5cfe98dea5e9138b353ef83513631b6ef8d335c5
codewithirene567 <irenejinscott@gmail.com>
2020-12-03 19:15:48 -0500

    added cards.js file JSX

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,1 +1,20 @@
 //hold all the cards
+import React from 'react'
+import CardItem from './CardItem'
+
+function Cards(){
+    return (
+        <div className='cards'>
+            <h1>Check out these EPIC Destinations!</h1>
+            <div className="cards_container">
+                <div className="cards__wrapper">
+                    <ul className="cards__items">
+                        <CardItem />
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
